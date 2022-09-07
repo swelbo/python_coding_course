3299c4411fa2c2b2dec33ef7361e6799a501d770
Thomas Sewell <tsewell000@gmaail.com>
2022-09-07 18:07:14 +0100

    updates

---
+++ classes.py
@@ -0,0 +1,13 @@
+
+from enum import IntEnum
+
+class x(IntEnum): # Action here could be anything. This class allows us to add or remove samples.  
+    test = 0 
+    this = 1
+    out = 2
+
+s = int(input("Please guess a number between 1 and 10 "))
+
+y = x(s)
+
+print(y.name)
--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -4,92 +4,85 @@
 
 import time
 import random
+from enum import IntEnum
 
 # welcome staatement
 print("Welcome to the rock paper scissors game!")
 x = input("Have you played before? ")
 
 if x == "yes" and "Yes" and "Yeah" and "yeah": 
-	print("Great! Let's begin")
+	print("Great! Let's see your worth!")
 else:
-	print("OK, here are the rules! \nThe Rules: \nYou must defend your honour by selecting either Rock, Paper or Scissors  asss your weapon!")
+	print("OK, here are the rules! \n\nThe Rules: \nYou must defend your honour by selecting either Rock, Paper or Scissors as your weapon!")
 
-time.sleep(2)
+time.sleep(1)
 
-print("OK, great! Let's get ready to begin")
+print("Let's get ready to play in:")
 
 # countdown to the start of game
-countdown = 5
+countdown = 3
 
+# a while loop that counts down from countdown every 0.5 seconds
 while countdown > 0:
 	print(countdown)
 	countdown -= 1
-	time.sleep(1)
+	time.sleep(0.5)
 
-# loop over the game
-while True:
+# define actions in a class. This class allows us to add or remove samples. 
+class Action(IntEnum): # Action here could be anything.  
+    Rock = 0 
+    Paper = 1
+    Scissors = 2
 
-# players choice
-    user_action = input("Player one, you're up! Rock, Paper or Scissors? ")
+# this function gets the users selection. It first asks for choices. Choices can be added or removed by editing the
+# class Action 
+def get_user_selection():
+    choices = [f"{action.name}[{action.value}]" for action in Action] # editdable choices linked to action
+    choices_str = ", ".join(choices) # removed square brackets (maybe does something else)
+    selection = int(input(f"Enter a choice ({choices_str}): ")) # user selection that neatly uses choices
+    action = Action(selection)
+    return action
 
-    # computers choice - uses random on a list of options. In this case its: rock paper scissors
-    possible_actions = ["Rock", "Paper", "Scissors"]
-    computer_action = random.choice(possible_actions)
 
-    # pause for effect
-    print("Hmm, intresting...")
-    time.sleep(3)
+# function to generate the computers selection
+def get_computer_selection():
+    selection = random.randint(0, len(Action) - 1)
+    action = Action(selection)
+    return action
 
-    # output user
-    print(f"The computer has chosen {computer_action} ... and you chose {user_action}!")
 
-    # compaare outputs - not using if elif else block
+# function to determine the winner
+def determine_winner(user_action, computer_action):
     if user_action == computer_action:
-        print(f"Both players selected {user_action}. It's a tie!")
-    elif user_action == "Rock":
-        if computer_action == "Scissors":
+        print(f"Both players selected {user_action.name}. It's a tie!")
+    elif user_action == Action.Rock:
+        if computer_action == Action.Scissors:
             print("Rock smashes Scissors! You win!")
         else:
             print("Paper covers Rock! You lose.")
-    elif user_action == "Paper":
-        if computer_action == "Rock":
+    elif user_action == Action.Paper:
+        if computer_action == Action.Rock:
             print("Paper covers Rock! You win!")
         else:
             print("Scissors cuts Paper! You lose.")
-    elif user_action == "Scissors":
-        if computer_action == "Paper":
+    elif user_action == Action.Scissors:
+        if computer_action == Action.Paper:
             print("Scissors cuts Paper! You win!")
         else:
             print("Rock smashes Scissors! You lose.")
 
-    play_again = input("Would you like another game? (y/n)")
-    if play_again != "y":
+
+while True:
+    try:
+        user_action = get_user_selection()
+    except ValueError as e:
+        range_str = f"[0, {len(Action) - 1}]"
+        print(f"Invalid selection. Enter a value in range {range_str}")
+        continue
+
+    computer_action = get_computer_selection()
+    determine_winner(user_action, computer_action)
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
         break
-
-
-
-''' # This is the practice python method.
-def compare(u1, u2):
-    if u1 == u2:
-        return("It's a tie!")
-    elif u1 == 'rock':
-        if u2 == 'scissors':
-            return("Rock wins!")
-        else:
-            return("Paper wins!")
-    elif u1 == 'scissors':
-        if u2 == 'paper':
-            return("Scissors win!")
-        else:
-            return("Rock wins!")
-    elif u1 == 'paper':
-        if u2 == 'rock':
-            return("Paper wins!")
-        else:
-            return("Scissors win!")
-    else:
-        return("Invalid input! You have not entered rock, paper or scissors, try again.")
-        sys.exit()
-
-print(compare(u1, u2))
-'''
