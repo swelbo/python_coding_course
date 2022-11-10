6d55cdae096d97e276b8ed46e4f7fc667129651e
swelbo <tsewell000@gmail.com>
2022-11-10 08:28:09 +0000

    updates

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+# call a dictinary 
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again." 
+}
+
+# Retrieving items from dictionary
+print(programming_dictionary["Bug"])
+
+# Adding new items to dictionary
+programming_dictionary["Loop"] = "yes"
+
