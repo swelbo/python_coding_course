b432af3595de0debaed8c3f180678275b7874a3b
swelbo <tsewell000@gmail.com>
2022-11-18 17:31:17 +0000

    final silent auction

--- silent_auction.py
+++ silent_auction.py
@@ -3,48 +3,43 @@ from replit import clear
 from art import logo2
 import time
 
-'''# print art
-print(logo2)'''
+def find_highest_biddder(bidding_record):
+    winner = ""
+    highest_bid = 0
+    for bidder in dictionary_of_bidders:
+        bid = int(dictionary_of_bidders[bidder])
+        if bid > highest_bid:
+            highest_bid = bid
+            winner = bidder
+    return winner
 
 # The dictionary of bidder
 dictionary_of_bidders = {}
-bools = True
-while bools == True:
 
+# while loop
+bidding_finished = False
+while not bidding_finished:
     # print art
     print(logo2)
-
     # Name input 
     name = input("what is your name? ")
-
     # bid 
-    bid = input("What is your bid? ")
-
+    bid = int(input("What is your bid? $"))
     # any more bidders? 
     next_bidder = input("Are there any more bidders y/n? ")
-
     # add name and bid into the dictionary
     dictionary_of_bidders[name] = bid
-    
     # loop through the game until there are no more bidders
-    if next_bidder == "y":
-        bools = True
+    if next_bidder == "n":
+        bidding_finished = True
+        winner = find_highest_biddder(dictionary_of_bidders)
     else:
-        bools = False
-
+        bidding_finished = False
     # clear screen for secrecy
     clear()
 
-# cycle through the dictionary replacing the winner with the highest bidder
-winner = ()
-
-highest_bid = 0
-for i in dictionary_of_bidders:
-    bid = int(dictionary_of_bidders[i])
-    if bid > highest_bid:
-        highest_bid = bid
-        winner = i
-    
-print(f"And the winner is . {time.sleep(0.1)} . {time.sleep(0.11)} . {time.sleep(0.1)} {winner}! ")
-
-
+# cycle through the dictionary replacing the winner with the highest bidder\
+print("And the winner is . . .") 
+time.sleep(0.5)
+print(winner.title())
+print(f"Congratulations, {winner.title()}")
