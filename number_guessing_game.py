2deea38db02be6651db53a147861af60ebd3fd86
swelbo <tsewell000@gmail.com>
2023-01-06 10:39:35 +0000

    guessing game etc..

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -10,43 +10,65 @@
 
 # libraries
 import random as rd
-import art
+from art import logo5
 import replit
 
-# welcome message
-print("Welcome to the number guessing game!")
+#function to check winner
+def check_winner(guess, random_number, attempts):
+    ''' Function that checks whether number is correct and returns attempts '''
+    if guess > random_number:
+        print("Nope! Too high!")
+        return attempts - 1
 
-# input variables
-random_number = rd.randint(0,100)
+    elif guess < random_number:
+        print("Nope! Too low!")
+        return attempts - 1
 
-play_again = "y"
-while play_again == "y":
+    else: 
+        print(f"Correct! The answer was {random_number}")
+        print(f"You completed the game with {attempts - 1} attempts remaining!")
+
+def set_diff():
+    ''' Function that sets level difficulty'''
     mode = input("  Please select a mode - easy/hard? - ").lower()
 
     if mode == "easy":
         attempts = 10
-    else:
+    elif mode == "hard":
         attempts = 5
 
-    # The game 
-    while attempts > 0:
-        guess = int(input("  Guess a number between 0-100 - "))
-        if guess > random_number:
-            print("Nope! Too high!")
-            attempts -= 1
-        elif guess < random_number:
-            print("Nope! Too low!")
-            attempts -= 1
-        else: 
-            print(f"Correct! The answer was {random_number}")
-            print(f"You completed the game with {attempts} attempts remaining!")
-            attempts = -1
+    return attempts
 
-    if attempts == 0:
-        print("Oh no you ran out of attempts")
-    
-    play_again = input("Would you like to play again - y/n? ")
+# welcome message
+def game():
+    ''' The game '''
+    print("Welcome to the number guessing game!")
 
-    if play_again == "n":
-        print("Goodbye.")
+    # input variables
+    random_number = rd.randint(0,100)
 
+    play_again = "y"
+    while play_again == "y":
+        
+        attempts = set_diff()
+
+        # The game 
+        guess = 0
+        while guess != random_number:
+            guess = int(input("  Guess a number between 0-100 - "))
+            attempts = check_winner(guess, random_number, attempts)
+            
+            if attempts != None:
+                print(f"You have {attempts} remaining")
+        
+        if attempts == 0:
+            print("Oh no you ran out of attempts")
+        
+        play_again = input("Would you like to play again - y/n? ")
+
+        if play_again == "n":
+            print("Goodbye.")
+    return
+
+# run game
+game()
